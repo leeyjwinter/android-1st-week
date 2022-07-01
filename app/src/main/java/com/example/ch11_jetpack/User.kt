9662a70430b82bcbb2a66bef64d5281d058ce169
Bang <jazzdosa@kaist.ac.kr>
2022-07-01 20:17:58 +0900

    0701_2017

--- app/src/main/java/com/example/ch11_jetpack/User.kt
+++ app/src/main/java/com/example/ch11_jetpack/User.kt
@@ -1,8 +1,8 @@
 package com.example.ch11_jetpack
 
 data class User(
-    //var name: String? = null,
-    var age: Int? = 0,
+    var datedb: String? = null
+    //var age: Int? = 0,
     //var email: String? = null,
     //var password: String? = null,
     //var isMarried: Boolean? = false
