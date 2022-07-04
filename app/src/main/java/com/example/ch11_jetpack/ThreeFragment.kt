7ee9c9c1877c24bc16b8d1fdb270ae4ced043be1
Bang <jazzdosa@kaist.ac.kr>
2022-07-04 13:22:53 +0900

    0702:1402

--- app/src/main/java/com/example/ch11_jetpack/ThreeFragment.kt
+++ app/src/main/java/com/example/ch11_jetpack/ThreeFragment.kt
@@ -12,6 +12,11 @@ import android.widget.Chronometer.OnChronometerTickListener
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.ch11_jetpack.databinding.FragmentThreeBinding
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+import kotlinx.android.synthetic.main.fragment_three.*
 import java.time.LocalDate
 import java.time.Duration
 import java.time.format.DateTimeFormatter
@@ -34,6 +39,56 @@ class ThreeFragment : Fragment() {
     ////SharedPreferences
     private val sharedManager: SharedManager by lazy { SharedManager(requireActivity().applicationContext) }
 
+    fun drawsimpleGraph(d6:Int, d5:Int, d4:Int, d3:Int, d2:Int, d1:Int, d0:Int){ // 그래프를 그려주는 함수
+    //fun drawsimpleGraph(){ // 그래프를 그려주는 함수
+
+        val entries: ArrayList<BarEntry> = ArrayList()
+
+        //for(i: Int in 0..6){
+        //    entries.add(BarEntry(i.toFloat(), time_table[i].toFloat()))
+        //}
+        entries.add(BarEntry(1f, d6.toFloat()))
+        entries.add(BarEntry(2f, d5.toFloat()))
+        entries.add(BarEntry(3f, d4.toFloat()))
+        entries.add(BarEntry(4f, d3.toFloat()))
+        entries.add(BarEntry(5f, d2.toFloat()))
+        entries.add(BarEntry(6f, d1.toFloat()))
+        entries.add(BarEntry(7f, d0.toFloat()))
+
+        val barDataSet = BarDataSet(entries, "")
+        barDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+
+        val datas = BarData(barDataSet)
+        binding.barChart.data = datas
+
+
+        //hide grid lines
+        binding.barChart.axisLeft.setDrawGridLines(false)
+        binding.barChart.axisLeft.setDrawLabels(false)
+        binding.barChart.xAxis.setDrawGridLines(false)
+        binding.barChart.xAxis.setDrawAxisLine(false)
+        // 축 레이블 숨기기
+        binding.barChart.xAxis.setDrawLabels(false)
+        //binding.barChart.getAxis.setDrawLabels(false)
+
+        //remove right y-axis
+        binding.barChart.axisRight.isEnabled = false
+
+        //remove legend
+        binding.barChart.legend.isEnabled = false
+
+
+        //remove description label
+        binding.barChart.description.isEnabled = false
+
+
+        //add animation
+        //binding.barChart.animateY(3000)
+
+
+        //draw chart
+        binding.barChart.invalidate()
+    }
 
 
     override fun onCreateView(
@@ -72,7 +127,7 @@ class ThreeFragment : Fragment() {
         var now = LocalDate.now()
 
         // base 날짜인 2022-06-20 설정하기
-        val strdate = "2022-06-25"
+        val strdate = "2022-06-26"
         val basedate = LocalDate.parse(strdate, DateTimeFormatter.ISO_DATE) // localdate형식으로 변환
         //Log.d("오늘 날짜", "go"+ ChronoUnit.DAYS.between( basedate , now ))
         //Log.d("날짜", "00000000".repeat(1000))
@@ -86,7 +141,8 @@ class ThreeFragment : Fragment() {
             val currentUser = User().apply {
                 //datedb = "00000000".repeat(1000)
                 //임시로 만든 0625~0630 데이터
-                datedb = "012152410141234102153412014512340112333304211111" + "00000000".repeat(994)
+                Log.d("퇴근??", "O")
+                datedb = "112152412141234112153412114512341912333334211111" + "00000000".repeat(994)
             }
             sharedManager.saveCurrentUser(currentUser)
         }
@@ -99,7 +155,7 @@ class ThreeFragment : Fragment() {
 
         ///////////////
         /////////현재 시간을 text로 간단하게 출력해보기!/////////////////////
-        fun StringToTime(str_time: String): String{
+        fun StringToTime(str_time: String): String{ // 시간을 스트링으로
             val my_time = str_time.toInt()
             val h = ((my_time / 3600000).toInt())
             val m = ((my_time - h * 3600000).toInt() / 60000)
@@ -111,14 +167,23 @@ class ThreeFragment : Fragment() {
             return "$hh $mm $ss "
         }
 
-        fun viewWeekTime(w_idx: Int){
+        fun viewWeekTime(w_idx: Int){ // 한 주 시간을 다 보여주기
             var weekTime = ""
             for(i: Int in -6..0) {
                 weekTime += "i is $i: "
                 weekTime += StringToTime(fulldata!!.substring((w_idx+i)*8, (w_idx+i+1)*8))
                 weekTime += "\n"
             }
-            binding.testTime.text = weekTime
+
+            // draw 진행해보기! (그래프 그리기)
+            var time_table = arrayOf(0, 0, 0, 0, 0, 0, 0)
+            for(i: Int in -6..0) {
+                time_table[i+6] = (fulldata!!.substring((w_idx+i)*8, (w_idx+i+1)*8).toInt() / 1000)
+            }
+            drawsimpleGraph(time_table[0], time_table[1], time_table[2], time_table[3], time_table[4], time_table[5], time_table[6])
+            //drawsimpleGraph()
+
+            //binding.testTime.text = weekTime
         }
 
         viewWeekTime(idx)
@@ -134,17 +199,23 @@ class ThreeFragment : Fragment() {
 
                 var now = LocalDate.now()
 
-                //Log.d("0625", "time"+fulldata!!.substring((idx-6)*8, (idx-5)*8))
-                //Log.d("0626", "time"+fulldata!!.substring((idx-5)*8, (idx-4)*8))
-                //Log.d("0627", "time"+fulldata!!.substring((idx-4)*8, (idx-3)*8))
-                //Log.d("0628", "time"+fulldata!!.substring((idx-3)*8, (idx-2)*8))
-                //Log.d("0629", "time"+fulldata!!.substring((idx-2)*8, (idx-1)*8))
-                //Log.d("0630", "time"+fulldata!!.substring((idx-1)*8, (idx-0)*8))
-                //Log.d("0701", now.toString()+"time"+fulldata!!.substring((idx-0)*8, (idx+1)*8))
+                //draw graph!
+
+
+                Log.d("0625", "time"+fulldata!!.substring((idx-6)*8, (idx-5)*8))
+                Log.d("0626", "time"+fulldata!!.substring((idx-5)*8, (idx-4)*8))
+                Log.d("0627", "time"+fulldata!!.substring((idx-4)*8, (idx-3)*8))
+                Log.d("0628", "time"+fulldata!!.substring((idx-3)*8, (idx-2)*8))
+                Log.d("0629", "time"+fulldata!!.substring((idx-2)*8, (idx-1)*8))
+                Log.d("0630", "time"+fulldata!!.substring((idx-1)*8, (idx-0)*8))
+                Log.d("0701", now.toString()+"time"+fulldata!!.substring((idx-0)*8, (idx+1)*8))
                 viewWeekTime(idx)
                 if(savetimeString.toLong() > fulldata!!.substring(idx*8, idx*8+8).toLong()){
                     datedb = fulldata!!.substring(0, idx*8) + savetimeString + fulldata!!.substring(idx*8+8, 1000*8) // 원하는 위치에 데이터 저장
                     fulldata = datedb
+                }
+                else{
+                    datedb = fulldata
                 }
             }
             sharedManager.saveCurrentUser(currentUser)
@@ -206,6 +277,8 @@ class ThreeFragment : Fragment() {
         }
         binding.submitLeaveButton.setOnClickListener{
 
+
+
             var now = LocalDate.now()
             var cidx = ChronoUnit.DAYS.between( basedate , now ).toInt() // 날짜를 계속 갱신!
             if(fulldata!!.substring((cidx-0)*8, (cidx+1)*8).equals("00000000")){
@@ -231,6 +304,8 @@ class ThreeFragment : Fragment() {
             println(min)
             println(ampm)
         }
+
+
 
         return binding.root
     }
